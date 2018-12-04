396cbda732884e80ba8e0b3b84bd423cf88f1352
Alex Wang <w348729366@gmail.com>
2018-12-04 23:39:40 +0800

    -add my personal statement

---
+++ mine.py
@@ -0,0 +1,128 @@
+
+
+# assume this python environment
+
+class PersonalStatement(object):
+    def __init__(self):
+        self.name = 'wang ruixue'
+        self.nationality = 'China'
+        self.birth_day = '1990-05-04'
+        self.gender = 'male'
+        self.job_title = 'backend developer'
+        self.company = 'Mafint(fintech company in singapore) '
+        self.company_target = 'deliver a financial product'
+        self.goal = 'going further in IT filed'
+        self.it_journey = []
+        self.daily_task = []
+        self.description = ''
+    def my_it_journey(self):
+        for each_journey in self.it_journey:
+            print(each_journey)
+        return 'list my it journey successfully'
+
+    @staticmethod
+    def describe_myself(description):
+        print(description)
+        return 'describe myself successfully'
+
+    def my_daily_task(self):
+
+
+public class Wander {
+
+        public static void main(String[] args){
+            Wander wander = new Wander();
+            wander.lookingForJob( / * add your interest here * / );
+            }
+
+        private boolean yourSkills(String skill) {}
+
+        public void lookingForJob(Interest interest) {
+
+        if (interest != null) {
+
+        if ( yourSkills("Develop capabilities for the hottest tech: iOS, Android, nodeJS, etc.") & &
+
+            yourSkills("Work with casual dress, no face time, no politics, get-stuff-done culture") & &
+
+            yourSkills("Got a passion for programming and a team mindset"))
+
+            {
+        System.out.println("Apply to Wander!\n\nWe are a design and technology studio positioned at the intersection " + "of 3 dominant trends in digital today: UX/UI, Development, Data Science.");
+
+        System.out.println("\nWe are looking for: \n");
+
+        // mobile person
+
+        if (interest == Interest.MOBILE) {
+
+        skillsCore();
+
+        skillsMobile();
+
+        // Backend...but not like that
+
+        } else if (interest == Interest.BACKEND)
+
+        skillsCore();
+
+        skillsBackend();
+
+        }
+
+        }
+
+        }
+
+        else {
+
+        throw new IllegalArgumentException"Sorry we have no job openings at the moment!");
+
+        }
+
+        }
+
+        // Bonus: identify
+        the
+        compilation
+        error(s) in this
+        ad.
+
+            private void skillsCore()
+        {
+            System.out.println("Passion for UX/UI, Development, Data Science.");
+            System.out.println("Enthusiasm to learn new  things.");
+            System.out.println("Sense of humour.");
+
+        }
+
+        private void skillsMobile()
+        {
+
+            System.out.println("Fluency in Swift or Java.");
+            System.out.println("Experience in Android or iOS development.");
+            System.out.println("ReactiveX experience is a huge plus.");
+        }
+
+        private void skillsBackend()
+        {
+            System.out.println("Java fluency required.");
+            System.out.println("Expertise in any of the following a big plus: JBoss, Spring, Hibernate, Gradle.");
+
+        }
+
+        public enum Interest
+        {
+            MOBILE,
+            BACKEND
+        }
+        }
+        Job Type: Full - time
+
+        Required experience:
+
+        Development: 1year
+        Job Location:Singapore
+        Required language:
+
+        English
