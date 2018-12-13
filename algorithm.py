019998cfdace587b52221a9db866fd0c66d8ca1b
Alex Wang <w348729@163.com>
2018-12-13 09:21:48 +0800

    Update algorithm.py
    -add Queue example

--- algorithm.py
+++ algorithm.py
@@ -101,3 +101,33 @@ def divide_by_2(dec_number):
         bin_string = bin_string + str(rem_stack.pop())
 
     return bin_string
+# 6. Queue
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+def hotPotato(namelist, num):
+    simqueue = Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+
+    while simqueue.size() > 1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+
+        simqueue.dequeue()
+
+    return simqueue.dequeue()
